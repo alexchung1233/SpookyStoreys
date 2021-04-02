f1ab3dbc88d141642602ca945973a0549579a34a
Carly-Klebine <carlyk9@gmail.com>
2021-04-01 23:14:51 -0400

    Should allow for switching between the three boxes on the menu file. Screen will flicker. Needs to be fixed.

---
+++ Debug/CMakeFiles/SpookyStoreys_core.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/Actor.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/Actor.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/GameView.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/GameView.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/ProcessManager.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/ProcessManager.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/menu.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/menu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Debug/CMakeFiles/SpookyStoreys_core.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpookyStoreys_core.dir/src/Actor.cpp.o"
+  "CMakeFiles/SpookyStoreys_core.dir/src/GameView.cpp.o"
+  "CMakeFiles/SpookyStoreys_core.dir/src/ProcessManager.cpp.o"
+  "CMakeFiles/SpookyStoreys_core.dir/src/menu.cpp.o"
+  "libSpookyStoreys_core.a"
+  "libSpookyStoreys_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpookyStoreys_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
