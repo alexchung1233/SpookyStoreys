1ce14f4e1d67dbd8ec5d7d17bf9a8e5d387e346f
alexchung1233 <awchung@email.wm.edu>
2021-04-09 15:08:19 -0400

    Added files

--- include/Actor.h
+++ include/Actor.h
@@ -1,11 +1,11 @@
 #ifndef ACTOR_H // include guard
 #define ACTOR_H
-#include "ActorComponent.h"
 #include "Position.h"
 
 class Actor
 {
 public:
+  Actor(int actorId) {actorId = id;}
   virtual void init();
   virtual bool update(float deltaMs);
   virtual void destroy();
