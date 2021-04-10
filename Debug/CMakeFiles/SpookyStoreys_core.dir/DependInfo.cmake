c42f5a33ee153e2fcb4186491afcbff3155ebf88
Carly-Klebine <carlyk9@gmail.com>
2021-04-10 13:52:11 -0400

    rendered holy water sprite

---
+++ Debug/CMakeFiles/SpookyStoreys_core.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/Actor.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/Actor.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/Game.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/Game.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/GameView.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/GameView.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/InputManager.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/InputManager.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/MonsterActor.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/MonsterActor.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/PlayerActor.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/PlayerActor.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/Position.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/Position.cpp.o"
+  "/Users/carlyklebine 1/Desktop/SpookyStoreys/src/ProcessManager.cpp" "/Users/carlyklebine 1/Desktop/SpookyStoreys/Debug/CMakeFiles/SpookyStoreys_core.dir/src/ProcessManager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
