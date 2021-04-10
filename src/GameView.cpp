4234733774176fa999881d0a8ebbb265c6b01127
Carly-Klebine <carlyk9@gmail.com>
2021-04-10 13:31:40 -0400

    Renders player sprite in the center of the screen

--- src/GameView.cpp
+++ src/GameView.cpp
@@ -22,14 +22,34 @@ void GameView::setup(){
 
 void GameView::update(sf::Event Event){
   sf::Texture texture;
+  sf::Texture texture_player;
+
   string test_level = "../data/bedroom_level_V2.png";
+  string player_file = "../data/protag_V1.png";
+
+
   if(!texture.loadFromFile(test_level)){
     printf("incorrect file format");
   }
+
+  if(!texture_player.loadFromFile(player_file)){
+    printf("incorrect file format");
+  }
+
   sf::Sprite sprite;
+  sf::Sprite sprite_player;
+
   this->App->clear();
+
   sprite.setTexture(texture);
   this->App->draw(sprite);
+
+  sprite_player.setTexture(texture_player);
+  sprite_player.setPosition(sf::Vector2f(400.f, 300.f));
+  sprite_player.setScale(sf::Vector2f(0.80f, 0.80f));
+
+  this->App->draw(sprite_player);
+
   inputManager.update(Event);
 
 
