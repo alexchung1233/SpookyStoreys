db9674d04ab0ae9bf8e8ba39e3439e684a9e8f41
Jackson Alexander Akers <jaakers@email.wm.edu>
2021-04-09 20:14:40 -0400

    Added playerActor move functions

--- include/PlayerActor.h
+++ include/PlayerActor.h
@@ -9,6 +9,13 @@ public:
   Position position;
   sf::Vector2f velocity;
   void move(float x, float y);
+  void move();
+  void resetVelocity();
+  void moveUp(float move);
+  void moveDown(float move);
+  void moveLeft(float move);
+  void moveRight(float move);
+
 };
 
 #endif
--- src/PlayerActor.cpp
+++ src/PlayerActor.cpp
@@ -10,3 +10,29 @@ void PlayerActor::move(float x, float y){
   position.x = x;
   position.y = y;
 }
+
+void PlayerActor::move(){
+  position.x += velocity.x;
+  position.y += velocity.y;
+}
+
+void PlayerActor::resetVelocity(){
+  velocity.x = 0;
+  velocity.y = 0;
+}
+
+void PlayerActor::moveUp(float move){
+  velocity.y -= move;
+}
+
+void PlayerActor::moveDown(float move){
+  velocity.y += move;
+}
+
+void PlayerActor::moveLeft(float move){
+  velocity.x -= move;
+}
+
+void PlayerActor::moveRight(float move){
+  velocity.x += move;
+}
