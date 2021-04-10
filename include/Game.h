ece698f0db86f2247aa642373715e5e33ea3edc5
alexchung1233 <awchung@email.wm.edu>
2021-04-10 11:38:33 -0400

    Added game.cpp

---
+++ include/Game.h
@@ -0,0 +1,43 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include "GameView.h"
+
+class Game
+{
+private:
+	sf::RenderWindow *window;
+	sf::Event event;
+
+  sf::Clock clock;
+	float dt;
+
+  GameView gameView;
+
+public:
+	Game();
+	//virtual ~Game();
+
+  void shutdownGame();
+
+
+  void initWindow();
+  void initVariables();
+  void initGameView();
+
+	//Update
+	void updateDt();
+	void update();
+
+	//Render
+	void render();
+
+	//Core
+	void runGame();
+};
+
+#endif
