72038dd5806815503a0cbea951f08aa9dc674802
Carly <36356930+Carly-Klebine@users.noreply.github.com>
2021-04-13 13:14:33 -0400

    Update ItemActor indents

--- include/ItemActor.h
+++ include/ItemActor.h
@@ -6,30 +6,30 @@
 #include "PlayerActor.h"
 
 class ItemActor : public Actor {
-   public:
-    	// pure virtual function
+	public:
+		// pure virtual function
 
-   		//gets the position of the item
-    	virtual Position getPosition() = 0;
+		//gets the position of the item
+		virtual Position getPosition() = 0;
 
-    	//sets myDialogue to be equal to dialogue
-    	virtual void setDialogue(string dialogue) = 0;
+		//sets myDialogue to be equal to dialogue
+		virtual void setDialogue(string dialogue) = 0;
 
-    	//returns myDialogue
-    	virtual string getDialogue() = 0;
+		//returns myDialogue
+		virtual string getDialogue() = 0;
 
-    	//checks to see if the player is next to/near the item (using nextToPlayer helper function)
-    	//and, if they are, then returns the proper dialogue (if any) and runs the appropriate
-    	//actions, such as checking if the player has a key, if the ItemActor should be destroyed,
-    	//and so on
-    	virtual string interact(PlayerActor player) = 0;      
+		//checks to see if the player is next to/near the item (using nextToPlayer helper function)
+		//and, if they are, then returns the proper dialogue (if any) and runs the appropriate
+		//actions, such as checking if the player has a key, if the ItemActor should be destroyed,
+		//and so on
+		virtual string interact(PlayerActor player) = 0;      
 	
 	protected:
 		//helper function for interact()
 		//returns true if the item is next to the player, false if otherwise
-     	virtual bool nextToPlayer(PlayerActor player) = 0;
+     		virtual bool nextToPlayer(PlayerActor player) = 0;
     	
-    	string myDialogue;
+    		string myDialogue;
 
 	private:
 };
