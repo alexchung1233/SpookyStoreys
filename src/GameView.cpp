671a1726a7c4bd28c257edb4f72ecb3fab1fe313
alexchung1233 <awchung@email.wm.edu>
2021-04-13 13:17:36 -0400

    Pushed changes

--- src/GameView.cpp
+++ src/GameView.cpp
@@ -17,29 +17,66 @@ GameView::GameView(sf::RenderWindow& app){
 
 void GameView::setup(){
   string test_level = "../data/bedroom_level_V2.png";
+  string player_file = "../data/protag_V1.png";
 
   if(!texture.loadFromFile(test_level)){
     printf("incorrect file format");
   }
 
+
+  if(!texture_player.loadFromFile(player_file)){
+    printf("incorrect file format");
+  }
   sprite.setPosition(logic->getPlayer().getPosition().x,
   logic->getPlayer().getPosition().y);
   sprite.setTexture(texture);
+  sprite_player.setTexture(texture_player);
+
+  sprite_player.setPosition(sf::Vector2f(400.f, 300.f));
+  sprite_player.setScale(sf::Vector2f(0.80f, 0.80f));
 
 }
 
 
 
 void GameView::update(sf::Event Event){
+  this->App->clear();
+  while(App->pollEvent(Event))
+  {
+    // Exit
+    if(Event.type == sf::Event::Closed) {
+      App->close();
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+    {
+      App->close();
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+      sprite_player.setPosition(sprite_player.getPosition().x
+      , sprite_player.getPosition().y-9.f);
+    }
 
-  App->clear();
-
-  inputManager.update(Event);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+      sprite_player.setPosition(sprite_player.getPosition().x
+      , sprite_player.getPosition().y+9.f);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+      sprite_player.setPosition(sprite_player.getPosition().x-9.f
+      , sprite_player.getPosition().y);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+      sprite_player.setPosition(sprite_player.getPosition().x+9.f
+      , sprite_player.getPosition().y);
+    }
+  }
 
   this->App->draw(sprite);
 
-
-
+  this->App->draw(sprite_player);
 
 
 
