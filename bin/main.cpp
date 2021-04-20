8f9933d145d96dd108a4772afcab1e326a6602a0
Carly-Klebine <carlyk9@gmail.com>
2021-04-20 13:16:26 -0400

    Update main to support GameManager and delete extraneous files

--- bin/main.cpp
+++ bin/main.cpp
@@ -10,12 +10,8 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-<<<<<<< HEAD
-  
-  Game game;
-=======
+
   GameManager game;
->>>>>>> adding_game_states
   game.runGame();
 
   // Done.
