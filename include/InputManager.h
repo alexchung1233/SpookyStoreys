95d414d248259b65217103a25ee3b3ec3aeddaf0
Jackson Alexander Akers <jaakers@email.wm.edu>
2021-05-02 16:15:34 -0400

    Added holy water radius

--- include/InputManager.h
+++ include/InputManager.h
@@ -19,7 +19,7 @@ class InputManager
 
     InputManager(sf::RenderWindow &app, GameLogic &logic);
     void operator()(sf::RenderWindow &app, GameLogic &logic);
-    void update(sf::Event& Event, float dt);
+    void update(sf::Event& Event, float dt, float distance);
     int getPlayState();
 
 };
